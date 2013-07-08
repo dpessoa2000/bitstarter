543683eaee644826c4f3f6e09b3d7fef2ae67e5a
John Smith <example@stanford.edu>
2013-07-08 03:15:50 +0000

    Updated web.js again3

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
     fs.readFileSync('index.html', function(err, data) {
 	if (err) throw err;
 	buffer = data
-	response.send(buf.toString('utf-8',data));
+	response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
