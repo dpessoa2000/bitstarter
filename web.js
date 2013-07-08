74032ea1bdf7edbc821ea41d9fc4fdab72222b7b
John Smith <example@stanford.edu>
2013-07-08 03:19:25 +0000

    Updated web.js again3

--- web.js
+++ web.js
@@ -5,10 +5,14 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer("I'm a string!", "utf-8")
 
 app.get('/', function(request, response) {
-    fs.readFileSync('index.html', function(err, data) {
-	if (err) throw err;
-	buffer = data
-	response.send(buffer.toString());
+//    fs.readFileSync('index.html', function(err, data) {
+//	if (err) throw err;
+//	buffer = data
+//	response.send(buffer.toString());
+
+    response.send('Hello World again');
+
+
 });
 
 var port = process.env.PORT || 5000;
