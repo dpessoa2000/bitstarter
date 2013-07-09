e069d4e0cd2a1a594a87b9d0f0169f51cbf594c8
John Smith <example@stanford.edu>
2013-07-09 04:35:22 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -6,12 +6,9 @@ var fs = require('fs');
 var buf = new Buffer("This is a string", "utf-8");
 
 app.get('/', function(request, response) {
-
-
     buf = fs.readFileSync('index.html');
-    response.send(buf.toString('utf-8'));
-
-
+//    response.send(buf.toString('utf-8'));
+    console.log(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
