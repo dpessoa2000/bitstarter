4e23066941759e4b72c16af753febc52d9e95ee9
John Smith <example@stanford.edu>
2013-07-09 04:24:48 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -3,35 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var buf = new Buffer("hello world from index.html", "utf-8");
+var buf = new Buffer("This is a string", "utf-8");
 
 app.get('/', function(request, response) {
-//    fs.readFileSync('index.html', function(err, data) {
-//	if (err) throw err;
-//	buffer = data
-//	response.send(buffer.toString());
 
-//    buffer = fs.readFileSync('index.html');
-
-//    buffer.write(fs.readFileSync('./index.html','utf8'));
-
-//    response.send('The buffer now is ' + buf.toString('utf8') + '\n');
-
-
-//    fs.readFileSync('/index.html', function(err, buf) {
-//	if (err) throw err;
-//	response.send(buf.toString('utf-8');
-//	});
 
     buf = fs.readFileSync('index.html');
     response.send(buf.toString('utf-8'));
-
-
-//    response.send('Below is from the buffer');
-
-//    response.send(buf.toString('utf-8'));
-
-//    response.send('Above is from the buffer.');
 
 
 });
